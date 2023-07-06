bfd7492a35860946232cf0dae7c21d591e7c58d8
kilpatrickap <kilpatrickap18@gmail.com>
2023-07-06 14:05:48 +0000

    new: Abstract_Dialog

--- Abstract.py
+++ Abstract.py
@@ -1,4 +1,6 @@
 import os
+import sqlite3
+
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
@@ -153,3 +155,89 @@ class Abstract_Dialog(object):
         self.tableWidget_takeOff.setColumnWidth(8, 170)
 
         self.pushButton_cancel.setText(_translate("Dialog", "Cancel"))
+
+        # Run load_tabel_data method
+        self.load_table_data()
+
+    def load_table_data(self):
+        # Connect to the SQLite database
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+
+        # Get the list of table names from the database
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables = cursor.fetchall()
+
+        # Extract table names from the fetched data and store them in a list
+        table_list = [table[0] for table in tables if table[0] != 'sqlite_sequence']  # Exclude the 'sqlite_sequence'
+
+        # print(table_list)
+
+        # Initialize an empty list to store all the retrieved data
+        all_data = []
+
+        # Iterate over each table in table_list
+        for table_name in table_list:
+            # Retrieve the data from the table
+            cursor.execute(f'SELECT * FROM {table_name}')
+            data = cursor.fetchall()
+
+            # Append the retrieved data to the all_data list
+            all_data.extend(data)
+
+        # print(all_data)
+
+        # Check if the all_data list is empty
+        if not all_data:
+            return
+
+        # Set the number of rows and columns in the QTableWidget
+        self.tableWidget_takeOff.setRowCount(len(all_data))
+        self.tableWidget_takeOff.setColumnCount(9)  # Column count set to 9
+
+        # Populate the QTableWidget with the retrieved data
+        for row_num, row_data in enumerate(all_data):
+            square_value = 0.0  # Initialize square_value
+            sign_post_value = None  # Initialize sign_post_value
+
+            for col_num, col_data in enumerate(row_data):
+                if col_num == 0:  # Exclude ID column
+                    continue
+
+                item = QtWidgets.QTableWidgetItem()
+
+                if col_num == 7:  # "square" column is at index 7
+                    try:
+                        square_value = float(col_data)
+                        formatted_value = '{:,.2f}'.format(square_value)
+                    except ValueError:
+                        formatted_value = str(col_data)
+                else:
+                    formatted_value = str(col_data)
+
+                item.setText(formatted_value)
+                self.tableWidget_takeOff.setItem(row_num, col_num - 1, item)  # Adjust column index
+
+                if col_num == 9:  # "sign_post" column is at index 9
+                    sign_post_value = str(col_data)  # Store the value of the sign_post column
+
+            # Check if square value is negative and set row color to red
+            if square_value < 0:
+                for col in range(self.tableWidget_takeOff.columnCount()):
+                    cell_item = self.tableWidget_takeOff.item(row_num, col)
+                    cell_item.setForeground(QtGui.QColor('red'))
+
+            # Check if sign_post_value == "SUM", set row foreground to blue
+            if sign_post_value == "SUM":
+                for col in range(self.tableWidget_takeOff.columnCount()):
+                    cell_item = self.tableWidget_takeOff.item(row_num, col)
+                    cell_item.setForeground(QtGui.QColor('blue'))
+
+            # Check if sign_post_value == "SUM", set row foreground to blue
+            if sign_post_value == "TONNAGE":
+                for col in range(self.tableWidget_takeOff.columnCount()):
+                    cell_item = self.tableWidget_takeOff.item(row_num, col)
+                    cell_item.setForeground(QtGui.QColor('green'))
+
+        # Close the database connection
+        conn.close()
