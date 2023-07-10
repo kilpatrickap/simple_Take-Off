e66ce0897a3dc6daf09a95b60d3b50b42c65f096
kilpatrickap <kilpatrickap18@gmail.com>
2023-07-10 12:07:14 +0000

    fix: populate project tree

--- ProjectTreeWidget.py
+++ ProjectTreeWidget.py
@@ -27,7 +27,7 @@ class Project_Widget(QtWidgets.QWidget):
         The specified folder path is set as the text of the root item in the tree widget. The method then calls the
         _add_folder_contents method to recursively add the contents of the folder as child items under the root item.
 
-        :param folder_path:
+        :param folder_path: The path of the folder whose contents should be displayed.
         :return: None
         """
 
@@ -39,6 +39,26 @@ class Project_Widget(QtWidgets.QWidget):
         self._add_folder_contents(folder_path, root_item)
 
     def _add_folder_contents(self, folder_path, parent_item):
+        """
+        Recursively adds the contents of a folder to a tree widget.
+
+        This private method is called by the `display_folder_contents` method to add the contents of a specified
+        folder to a tree widget. It recursively traverses through the folder structure and adds each file and
+        subfolder as items to the tree widget, under the provided parent item.
+
+        The method begins by iterating over each entry (file or subfolder) in the specified folder using `os.listdir`.
+        For each entry, a new `QTreeWidgetItem` is created with the provided `parent_item` as its parent, and the entry
+        name is set as the text of the item.
+
+        If the entry is a directory, the method calls itself recursively, passing the entry's path as the new
+        `folder_path` and the newly created item as the new `parent_item`. This allows for the recursive traversal of
+        subfolders and the addition of their contents to the tree widget.
+
+        :param folder_path: The path of the folder whose contents should be added.
+        :param parent_item: The parent item under which the folder contents should be added.
+        :return:
+        """
+
         for entry in os.listdir(folder_path):
             entry_path = os.path.join(folder_path, entry)
 
