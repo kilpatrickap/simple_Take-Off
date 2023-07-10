aaa4db1ddb53af0e20f6dd31cfab212a17e6eafc
kilpatrickap <kilpatrickap18@gmail.com>
2023-07-10 12:03:17 +0000

    fix: populate project tree

--- ProjectTreeWidget.py
+++ ProjectTreeWidget.py
@@ -11,10 +11,26 @@ class Project_Widget(QtWidgets.QWidget):
         self.treeWidget_project.setObjectName("treeWidget_project")
         self.treeWidget_project.setHeaderLabels(["Project Folder"])
 
+        # Display folder when app loads
+        self.display_folder_contents('/Users/kilpatrick/Documents/Trad Take-Off app/Simpe Take-Off')
+
         self.horizontalLayout = QtWidgets.QHBoxLayout(self)
         self.horizontalLayout.addWidget(self.treeWidget_project)
 
     def display_folder_contents(self, folder_path):
+        """
+        Populates the tree widget with the contents of the specified folder.
+
+        This method clears the existing contents of the tree widget and adds the contents of the specified folder as
+        items to the tree widget. Each item represents a file or sub folder within the specified folder.
+
+        The specified folder path is set as the text of the root item in the tree widget. The method then calls the
+        _add_folder_contents method to recursively add the contents of the folder as child items under the root item.
+
+        :param folder_path:
+        :return: None
+        """
+
         self.treeWidget_project.clear()
 
         root_item = QtWidgets.QTreeWidgetItem(self.treeWidget_project)
@@ -31,13 +47,3 @@ class Project_Widget(QtWidgets.QWidget):
 
             if os.path.isdir(entry_path):
                 self._add_folder_contents(entry_path, item)
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-
-    widget = Project_Widget()
-    widget.display_folder_contents("/path/to/project/folder")
-    widget.show()
-
-    sys.exit(app.exec())
