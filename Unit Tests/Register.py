dbdb14174c32ecf556d6e6368c131959344f0159
kilpatrickap <kilpatrickap18@gmail.com>
2023-07-26 11:38:46 +0000

    DONE: register

--- Unit Tests/Register.py
+++ Unit Tests/Register.py
@@ -66,7 +66,54 @@ class Ui_Dialog(object):
         key = key[:-1]   # Everything up to the last digit
 
         # Output the key
-        self.lineEdit_key.insert(key)
+        #self.lineEdit_key.insert(key)
+
+        # Verify
+        if self.verify(key):
+            # Key is verified
+            self.lineEdit_key.setText(key)
+            self.label_verify.setText("Valid!")
+            self.label_score.setText(f'Score: {score}')
+        else:
+            # Key is not verified
+            # Run the generate() function again
+            self.generate()
+
+    def verify(self, key):
+        global score
+        score = 0
+
+        # Define our check digit
+        check_digit = key[2]    # Say 3rd digit
+        check_digit_count = 0   # Track how many times we see that digit in the key
+
+        # aafa-bbfb-cccc-ddfd-1111
+        # separate by hypen
+        chunks = key.split('-')
+
+        # Loop through chunks and check
+        for chunk in chunks:
+            if len(chunk) != 4:     # If chunk is < 4, return false
+                return False
+
+            for char in chunk:
+                if char == check_digit:
+                    check_digit_count += 1  # add 1 to the counter
+
+                # Grab the score of the ANSCII char
+                score += ord(char)      # ord() converts the numerical value of a char.
+
+        # Check for rules
+        if 1700 < score < 1800 and check_digit_count == 3:
+            return True
+        else:
+            return False
+
+
+
+
+
+
 
 
 
