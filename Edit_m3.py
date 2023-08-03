f00cc06008b84cf8789ceb6f5e6a2e25729bae1b
kilpatrickap <kilpatrickap18@gmail.com>
2023-08-03 11:49:15 +0000

    FINAL: Edit_m3 Destroy & Insert dialog set to WindoModality & WindowStaysOnTopHint.

--- Edit_m3.py
+++ Edit_m3.py
@@ -667,6 +667,9 @@ class Edit_m3_Dialog(object):
         # Set the window modality to ApplicationModal
         msg_box.setWindowModality(QtCore.Qt.WindowModality.ApplicationModal)
 
+        # Set the WindowStaysOnTopHint flag
+        msg_box.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
+
         msg_box.setStandardButtons(
             QtWidgets.QMessageBox.StandardButton.Ok | QtWidgets.QMessageBox.StandardButton.Cancel
         )
@@ -703,6 +706,12 @@ class Edit_m3_Dialog(object):
             icon_path = os.path.join(os.path.dirname(__file__), "images", "exclamation-red.png")
             icon_pixmap = QtGui.QPixmap(icon_path)
             msg_box.setIconPixmap(icon_pixmap)
+
+            # Set the window modality to ApplicationModal
+            msg_box.setWindowModality(QtCore.Qt.WindowModality.ApplicationModal)
+
+            # Set the WindowStaysOnTopHint flag
+            msg_box.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
 
             msg_box.setStandardButtons(
                 QtWidgets.QMessageBox.StandardButton.Ok | QtWidgets.QMessageBox.StandardButton.Cancel
