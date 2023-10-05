24ac6b573a0295cf2cbae7c6f91946dd81c68d91
kilpatrickap <kilpatrickap18@gmail.com>
2023-10-05 12:15:42 +0000

    Responsive Tab_nr Sheet (if screen res < 1920x1080; reduce size of groupBox PROPORTIONATELY)

--- Tab_nr.py
+++ Tab_nr.py
@@ -189,6 +189,9 @@ class Tab_nr_Widget(QtWidgets.QWidget):
         self.horizontalLayout_11 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_11.setObjectName("horizontalLayout_11")
 
+        # Connect the resize event to the method for dynamic column widths
+        self.resizeEvent = self.on_resize
+
         # Horizontal spacer
         spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Policy.Expanding,
                                             QtWidgets.QSizePolicy.Policy.Minimum)
@@ -579,6 +582,46 @@ class Tab_nr_Widget(QtWidgets.QWidget):
 
         self.pushButton_nr_clear.setText(_translate("tabWidget_nr", "Clear"))
         self.pushButton_nr_insert.setText(_translate("tabWidget_nr", "Insert"))
+
+    def on_resize(self, event):
+        # Get the screen resolution
+        screen = QtWidgets.QApplication.primaryScreen()
+        screen_geometry = screen.geometry()
+        screen_width = screen_geometry.width()
+        screen_height = screen_geometry.height()
+
+        # Define the proportions for each column (half of the original width)
+        original_column_widths = [60, 40, 250, 60, 60, 60, 60, 30, 100]
+
+        if screen_width < 1920 or screen_height < 1080:
+            # Calculate the reduction factor based on screen resolution
+            reduction_factor = min(screen_width / 1920, screen_height / 1080)
+
+            # Reduce the column widths by applying the reduction factor
+            column_widths = [int(width * reduction_factor * 1.15) for width in original_column_widths]
+
+            # # Set the font size to 8
+            # font = QtGui.QFont()
+            # font.setPointSize(8)
+            # self.tableWidget_m2.setFont(font)
+
+            # Reduce the size of the groupBox geometry by applying the reduction factor
+            current_geometry = self.groupBox_nr.geometry()
+            new_width = int(current_geometry.width() * reduction_factor * 1.199)
+            new_height = int(current_geometry.height() * reduction_factor * 0.94)  # TODO: To make responsive
+            new_geometry = QtCore.QRect(current_geometry.x(), current_geometry.y(), new_width, new_height)
+            self.groupBox_nr.setGeometry(new_geometry)
+
+        else:
+            column_widths = original_column_widths
+
+        # Distribute the available width among columns proportionally
+        for col, width in enumerate(column_widths):
+            self.tableWidget_nr.setColumnWidth(col, width)
+
+        # Call the base class's resize event handler
+        super().resizeEvent(event)
+
 
     def save_table_data(self):
         # Import the return of self.code here as code_string
