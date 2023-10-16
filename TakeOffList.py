81c262e8d31ae14363bf262a9c92283a93573716
kilpatrickap <kilpatrickap18@gmail.com>
2023-10-16 18:01:37 +0000

    Persist completed and uncompleted Items. ==> DONE FOR childs and sub-items.

--- TakeOffList.py
+++ TakeOffList.py
@@ -342,3 +342,4 @@ class TakeOffList_Widget(QtWidgets.QWidget):
             # If the item has sub-items, recursively collect their data
             if item.childCount() > 0:
                 self.collect_item_data(item, item_data_list)
+
